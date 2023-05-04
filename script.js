06a0569e25a97375b94c55002c2482899bbc11ee
patrykmisiewicz <patrykmisiewicz1@gmail.com>
2023-05-04 20:51:18 +0200

    Chnage hover in style to event listener

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const container = document.querySelector('.sketchbox');
 
-
 for (i = 0; i < 256; i++) {
-    let content = document.createElement('div');
-    container.appendChild(content);
-    content.classList.add('pixel');
+  let content = document.createElement('div');
+  container.appendChild(content);
+  content.classList.add('pixel');
+  
+  content.addEventListener('mouseover', function(e) {
+    e.target.style.background = 'blue';
+  });
 }
