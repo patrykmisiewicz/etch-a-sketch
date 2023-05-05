115bfb3b4cddb4a94af99086bfe4aa4606e0da36
patrykmisiewicz <patrykmisiewicz1@gmail.com>
2023-05-05 13:45:28 +0200

    Add more buttons, logo and header

--- script.js
+++ script.js
@@ -2,43 +2,55 @@ const container = document.querySelector('.sketchbox');
 
 const btn = document.querySelector('.pixels');
 
+// colors variables
+
 const colorRed = document.querySelector('.colorRed');
 const colorGreen = document.querySelector('.colorGreen');
 const colorBlue = document.querySelector('.colorBlue');
 const colorCustom = document.querySelector('.colorCustom');
 
-
-const maxSize = 600;
+const clear = document.querySelector('.clear');
 
 let color = 'white';
 
+const maxSize = 600;
 
+// custom grid size function
 
 btn.addEventListener('click', function(e) {
     let i = prompt("Set grid size: ");
     grid(i);
 });
 
+// RED color button
+
 colorRed.addEventListener('click', function() {
     color =  'red';
     return;
 });
+
+// GREEN color button
 
 colorGreen.addEventListener('click', function() {
     color =  'green';
     return;
 });
 
+// BLUE color button
 
 colorBlue.addEventListener('click', function() {
     color =  'blue';
     return;
 });
 
+// CUSTOM color button
+
 colorCustom.addEventListener('click', function() {
     color =  prompt("Set color: ");
+    color = color.toLowerCase();
     return;
 });
+
 
 function grid(i) {
     container.innerHTML = "";
@@ -46,25 +58,30 @@ function grid(i) {
     const pixelWidth = Math.floor((maxSize - (i-1)*2)/i);
     const rowHeight = pixelWidth * i + (i-1)*2;
   
-    for (let j = 0; j < i; j++) {
-      const row = document.createElement("div");
-      row.style.display = "flex";
+    for (let j = 0; j < i; j++) {                    // Row function
+      const row = document.createElement('div');
+      row.style.display = 'flex';
       row.style.height = `${pixelWidth}px`;
       container.appendChild(row);
   
-      for (let k = 0; k < i; k++) {
-        const pixel = document.createElement("div");
+      for (let k = 0; k < i; k++) {                  // Pixel function
+        const pixel = document.createElement('div');
         pixel.style.width = `${pixelWidth}px`;
         pixel.style.height = `${pixelWidth}px`;
-        pixel.style.border = "1px solid black";
+        pixel.style.border = '1px solid black';
+        pixel.style.background = 'white'
         row.appendChild(pixel);
   
         pixel.addEventListener('mouseover', function(e) {
           e.target.style.background = color;
         });
+
+        clear.addEventListener('click', function() { // Clear function
+          pixel.style.background = 'white';
+        });
+
       }
     }
-  
     container.style.height = `${rowHeight}px`;
   }
   
